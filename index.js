4adf93e1a1eaf5bdcd7e64d22f3ef330bb37879d
Himanshu Singh <himanshusingh@real11.com>
2022-09-09 00:27:15 +0530

    database added

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+require('dotenv').config()
 const express = require('express')
+const User = require('./model/user')
+const { mongoConnect } = require('./mongo_config')
 
 const app = express()
 
+app.use('/getusers/:name', async (req,res)=>{
+    const name = req.params.name
+    const result = await User.create({username: name})
+    res.json({status: 'success', data: result})
+})
 
 app.use('/', (req,res)=>{
     res.send('port stated')
 })
+
 
 app.listen(5000, () => {
     console.log('app listing to port')
---
+++ mongo_config.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const mongoConnect = async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGO_URL}`);
+        console.log('Connection success');
+      } catch (error) {
+        console.log(error);
+    }
+}
+
+mongoConnect()
+
+module.exports = {
+    mongoConnect
+}
