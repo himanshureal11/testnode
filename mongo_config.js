397b1dd8de5561ad3245861bdc80d9c29791f23d
Himanshu Singh <himanshusingh@real11.com>
2022-09-09 01:41:58 +0530

    setting the connection by uri

--- mongo_config.js
+++ mongo_config.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const mongoConnect = async () => {
     try {
-        console.log(process.env.MONGO_URL, ">>>>>>");
         await mongoose.connect(process.env.MONGO_URL);
         console.log('Connection success');
       } catch (error) {
