58449342cc2c5874c02946fa94c8e4e5eed018bf
Himanshu Singh <himanshusingh@real11.com>
2022-09-09 01:13:36 +0530

    uri edit

--- mongo_config.js
+++ mongo_config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const mongoConnect = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect("mongodb+srv://test:hfPqe7Go1CNOYt5n@cluster0.yunvzxh.mongodb.net/?retryWrites=true&w=majority");
         console.log('Connection success');
       } catch (error) {
         console.log(error);
